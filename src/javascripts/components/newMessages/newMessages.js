89568f1bf19ca38d4f6f8081b55af436c3a82bd7
David Mullins <Mullins6323@gmail.com>
2019-05-04 11:35:43 -0500

    wip

--- src/javascripts/components/newMessages/newMessages.js
+++ src/javascripts/components/newMessages/newMessages.js
@@ -1,9 +1,26 @@
 import timeStamp from '../../helpers/timeStamp';
 import getMessages from '../getMessages/getMessages';
+// import multipleUsers from '../multipleUsers';
+
+const radios = document.getElementsByName('radio');
+
+const CurrentUser = () => {
+  radios.forEach((radio) => {
+    if (radio.checked) {
+      const user = radio.value;
+      return user;
+    }
+    return false;
+  });
+};
 
 const newMessage = () => {
   // TODO - username from radio button, 'name' is a placeholder
-  const newObj = { username: '7TenSplit', message: document.getElementById('commentSection').value, timeStamp: timeStamp.timeStamp() };
+  const newObj = {
+    username: CurrentUser(),
+    message: document.getElementById('commentSection').value,
+    timeStamp: timeStamp.timeStamp(),
+  };
   getMessages.addMessages(newObj);
   getMessages.domStringBuilder();
   document.getElementById('commentSection').value = '';
