9a2ca019596921288cff46de5e55e6a56eb9135b
David Mullins <Mullins6323@gmail.com>
2019-05-04 12:07:29 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,16 +1,11 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import timeStamp from './helpers/timeStamp';
-import multipleUsers from './components/multipleUsers';
 import divDomBuilder from './components/divDomBuilder/divDomBuilder';
 
 const init = () => {
   divDomBuilder.bodyDivCaller();
   timeStamp.timeStamp();
-  multipleUsers.multipleUsers();
-  attachEvents.attachButton();
-  attachEvents.attachClick();
-  attachEvents.clearButton();
 };
 
 init();
