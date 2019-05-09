25c500e33e0135055b362555c02abdcefd965285
David Mullins <Mullins6323@gmail.com>
2019-05-09 01:09:25 -0500

    messages are not overlapping buttons

--- src/javascripts/components/buttonsDiv/buttonsDiv.js
+++ src/javascripts/components/buttonsDiv/buttonsDiv.js
@@ -16,6 +16,17 @@ const btnDivBuilder = () => {
   domString += '      <span class="slider round"></span>';
   domString += '  </label>';
   domString += '</div>';
+  domString += '<div id="sideNav">';
+  domString += '  <h3 id="usersHeader">Users</h3>';
+  domString += '    <ul class="list-unstyled components">';
+  domString += '      <li><input type="radio" name="radio" class="userSelector" value="guest" id="guest">guest</input></li>';
+  domString += '      <li><input type="radio" name="radio" class="userSelector" value="joebiden666" id="joebiden666">joebiden666</input></li>';
+  domString += '      <li><input type="radio" name="radio" class="userSelector" value="lizwarren4u" id="lizwarren4u">lizwarren4u</li>';
+  domString += '      <li><input type="radio" name="radio" class="userSelector" value="axlrose" id="axlrose">axlrose</li>';
+  domString += '      <li><input type="radio" name="radio" class="userSelector" value="pantsuitgirl" id="pantsuitgirl">pantsuitgirl</li>';
+  domString += '      <li><input type="radio" name="radio" class="userSelector" value="rando_bumpkin" id="rando_bumpkin">rando_bumpkin</li>';
+  domString += '    </ul>';
+  domString += ' </div>';
   util.printToDom('btnDivPrint', domString);
   attachEvents.btnDivEvents();
   document.getElementById('darkOn').addEventListener('click', () => {
