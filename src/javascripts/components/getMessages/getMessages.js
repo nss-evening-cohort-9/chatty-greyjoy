addffab93cca3a8e8acdb2c5bc39b8346438041b
JPantana <joshpantana@me.com>
2019-05-09 17:16:57 -0500

    wip

--- src/javascripts/components/getMessages/getMessages.js
+++ src/javascripts/components/getMessages/getMessages.js
@@ -4,8 +4,9 @@ import './getMessages.scss';
 import $ from '../../../../node_modules/jquery';
 
 let messages = [];
-const countArr = [];
-const eventArr = [];
+// const countArr = [];
+// const eventArr = [];
+const messagesUpDown = [];
 const getMessages = () => messages;
 
 const addMessages = (newMessage) => {
@@ -15,15 +16,82 @@ const addMessages = (newMessage) => {
   }
 };
 
-const counterFn = (num) => {
-  countArr.push(num);
-  return num;
-};
-
-// const eventFn = (num) => {
-//   eventArr.push(num);
+// const counterFn = (num) => {
+//   countArr.push(num);
+//   // console.error(messages.forEach((message) => {
+//   //   console.error(message.id, 'hi');
+//   //   // console.error('hi');
+//   // }));
 //   return num;
 // };
+
+// const eventA = (num) => {
+//   console.error(num);
+//   return num;
+// };
+
+const messageLikes = [
+  {
+    userId: 1,
+    messageId: 'unique0',
+    up: false,
+    down: false,
+  },
+  {
+    userId: 2,
+    messageId: 'unique1',
+    up: false,
+    down: false,
+  },
+  {
+    userId: 1,
+    messageId: 'unique2',
+    up: false,
+    down: false,
+  },
+  {
+    userId: 4,
+    messageId: 'unique3',
+    up: false,
+    down: false,
+  },
+  {
+    userId: 5,
+    messageId: 'unique4',
+    up: false,
+    down: false,
+  },
+];
+
+// const likeFnEvent = (e) => {
+//   e.preventDefault();
+//   messages.forEach((message) => {
+//     const messageVotes = messageLikes.filter(msg => msg.messageId === message.id);
+//     const messageWithUpDown = Object.create(message);
+//     messageWithUpDown.upTotal = messageVotes.filter(msg => msg.up).length;
+//     console.error(messageWithUpDown.upTotal);
+//     messageWithUpDown.downTotal = messageVotes.filter(msg => msg.down).length;
+//     messagesUpDown.push(messageWithUpDown);
+//     // console.error(messageWithUpDown);
+//   });
+// if (messageLikes.find(m => m.userId === ['userId'] && m.messageId === e.target.closest())) {
+// if (messageLikes.filter(m => m.messageId === e.target.closest('i'))) {
+//   messageLikes.up = 0;
+// console.error(messageLikes);
+// update the old object in messageLike array
+// } else {
+// console.error(messageLikes, 'not working');
+// add a new object into messageLikeArray
+// }
+// };
+
+// if (messageLikes.find(m => m.userId === [userId] && m.messageId === e.target.closest(
+//  ['something that has the message Id on it'])) {
+//     // update the old object in messageLike array
+//   }
+// else {
+//  // add a new object into messageLikeArray
+// }
 
 const domStringBuilder = () => {
   let domString = '';
@@ -34,31 +102,35 @@ const domStringBuilder = () => {
     domString += `     <div class="userName">${message.username}</div>`;
     domString += `     <div class="cardBody">${message.message}</div>`;
     domString += `     <div class="timeStamp">${message.timeStamp}</div>`;
-    domString += `<div id="unique${countArr.length}" class="timeStamp">`;
-    domString += `<i id="unique${eventArr.length}" class="likes fa fa-thumbs-up"></i>  Likes`;
-    domString += '<a class="like">';
-    domString += `<input class="qty1" name="qty1" readonly="readonly" type="text" value="${countArr.length}"/>`;
-    domString += '</a>';
+    domString += `<div id="${message.id}" class="timeStamp">`;
+    domString += '<i class="likes fa fa-thumbs-up"></i>  Likes';
+    domString += '<p class="int">0</p>';
+    // .likes find 'closes'(#id) {change value from 0 to 1}
+    // value will need to be a function template literal? Gloably declared.
+    // domString += `<input class="qty1" name="qty1" readonly="readonly"
+    // type="text" value="${countArr.length}"/>`;
+    // domString += '</a>';
     domString += '</div>';
     domString += '    </div>';
     domString += '  </div>';
     domString += '</div>';
-    counterFn(1);
     util.printToDom('msgPrintingDiv', domString);
     $('.likes').click((e) => {
       e.preventDefault();
+      // const whatever = document.getElementsByClassName('.int').parseInt());
+      // counterFn(1);
+      messages.forEach((m) => {
+        const messageVotes = messageLikes.filter(msg => msg.messageId === m.id);
+        const messageWithUpDown = Object.create(message);
+        messageWithUpDown.upTotal = messageVotes.filter(msg => msg.up).length;
+        messageWithUpDown.downTotal = messageVotes.filter(msg => msg.down).length;
+        messagesUpDown.push(messageWithUpDown);
+      });
     });
   });
 };
 
 // messageLikes.filter(m => m.messageId === message.id && m.up).length
-
-// ```if (messageLikes.find(m => m.userId === [userId] && m.messageId ===
-// e.target.closest([something that has the message Id on it])) {
-//   // update the old object in messageLike array
-// } else {
-//  // add a new object into messageLikeArray
-// }```
 
 // --------------Change Font Start------------------
 
